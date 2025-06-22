cbeff0d2ff111f5b7cc91b8ac5f4e8448a632846
Nick Sweet <150301894+RainbowVegas@users.noreply.github.com>
2025-06-22 15:12:41 -0400

    Add files via upload

---
+++ NFLminingPandas.py
@@ -0,0 +1,55 @@
+import tweepy
+import pandas as pd
+
+# set up Twitter API credentials
+consumer_key = 'your consumer key here'
+consumer_secret = 'your secret consumer key here'
+access_token = 'your access token here'
+access_token_secret = 'your secret access token here'
+usernames = []
+twts = []
+
+# authenticate with Twitter API
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+# create API object
+api = tweepy.API(auth, wait_on_rate_limit=True)
+
+# define search query
+search_words = "Your Selected Team Terms Here"
+date_since = "2022-01-01"
+
+# perform the search
+tweets = tweepy.Cursor(api.search_tweets,
+              q=search_words,
+              lang="en",
+              since_id=date_since).items(2500)
+
+for tweet in tweets:
+    usernames.append(tweet.user.screen_name)
+    twts.append(tweet.text)
+
+#adjust search term    
+search_words = "New England Patriots"
+
+tweets = tweepy.Cursor(api.search_tweets,
+              q=search_words,
+              lang="en",
+              since_id=date_since).items(2500)
+
+for tweet in tweets:
+    usernames.append(tweet.user.screen_name)
+    twts.append(tweet.text)
+
+# Add mined tweets into dataset
+data = {
+    "username": usernames,
+    "tweets": twts
+}
+
+print(len(twts))
+print(len(usernames))
+
+df = pd.DataFrame(data)
+df.to_csv('PatriotsData.csv')
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# -NFL-Twitter-Data-Mining
-Sentiment analysis on over 5000 tweets for each of the 10 NFL teams analyzed 
+# NFLTwitterDataMining
+Sentiment analysis on over 5000 tweets for each of the 10 NFL teams analyzed
+
+# Authors
+Michael Hanlon Jr,
+Nicholas Sweet,
+Steven Liu,
+Rudolph DelFavaro,
+Graciela Avila,
+and Matthew Dickson
+
+
+# NFL Teams included in Analysis:
+Buffalo Bills,
+New York Giants,
+New England Patriots,
+Dallas Cowboys,
+Kansas City Chiefs,
+Las Vegas Raiders,
+Los Angeles Chargers,
+Arizona Cardinals,
+Detroit Lions, and
+Cleveland Browns
+
